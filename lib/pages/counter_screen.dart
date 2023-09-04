4c7baa9177ea6cfed38a52c0951d01468918bd35
Maria Isabel Chacon Rodriguez <isar509@gmail.com>
2023-09-04 15:08:57 -0600

    Agregación de dos botones

--- lib/pages/counter_screen.dart
+++ lib/pages/counter_screen.dart
@@ -8,34 +8,74 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
+  int contador = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Contador')),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('54',
-                style: TextStyle(
+            Text('$contador',
+                style: const TextStyle(
                     fontSize: 80,
                     fontWeight: FontWeight.w200,
-                    color: Colors.indigoAccent)),
-            Text('Número de clicks'),
+                    color: Color.fromARGB(255, 124, 69, 160))),
+            Text('clic${contador == 1 ? '' : 's'}',
+                style: const TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.w200,
+                    color: Color.fromARGB(255, 224, 147, 238))),
           ],
         ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
+          CustomButton(
+              icono: Icons.plus_one_rounded,
+              onPressed: () {
+                setState(() {
+                  contador++;
+                });
+              }),
           FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(
-              Icons.plus_one,
-            ),
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                if (contador == 0) return;
+                contador--;
+              });
+            },
+            child: const Icon(Icons.exposure_minus_1_rounded),
+          ),
+          FloatingActionButton(
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                contador = 0;
+              });
+            },
+            child: const Icon(Icons.restart_alt_rounded),
           )
         ],
       ),
     );
   }
-}
+} //fin clase _CounterScreenState
+
+class CustomButton extends StatelessWidget {
+  final IconData icono;
+  final VoidCallback? onPressed;
+  const CustomButton({super.key, required this.icono, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: onPressed,
+      child: Icon(icono),
+    );
+  }
+}//clase CustomButton
